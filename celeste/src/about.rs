31a52c253f12b318d354383bb5fe2c13de802586
Hunter Wittenborn <hunter@hunterwittenborn.com>
2023-09-16 14:35:22 -0500

    Add release notes to about page (#138)
    * Add release notes to about page
    
    * Remove unused dependency

--- celeste/src/about.rs
+++ celeste/src/about.rs
@@ -1,5 +1,7 @@
 use adw::{gtk::License, prelude::*, AboutWindow, Application};
 
+static METADATA: &str = include_str!(concat!(env!("OUT_DIR"), "/release.xml"));
+
 pub fn about_window(app: &Application) {
     let window = AboutWindow::builder()
         .application(app)
@@ -12,6 +14,7 @@ pub fn about_window(app: &Application) {
         ])
         .issue_url("https://github.com/hwittenborn/celeste")
         .license_type(License::Gpl30)
+        .release_notes(METADATA)
         .support_url("https://github.com/hwittenborn/celeste/issues")
         .build();
     window.add_credit_section(Some(&tr::tr!("App icons by")), &["Adrien Facélina"]);
